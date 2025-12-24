aed8380bb6792f426dbd222be92b876010d79ad7
Pooja Patel <50044663+PoojaPatel44@users.noreply.github.com>
2025-12-24 18:48:54 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,10 +27,11 @@ pd_df = my_dataframe.to_pandas()
 #st.stop()
                                                                       
 ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients:'
-    , my_dataframe
-    , max_selections = 5
+    'Choose up to 5 ingredients:',
+    pd_df['FRUIT_NAME'].tolist(),
+    max_selections=5
 )
+
 
 if ingredients_list:
     ingredients_string = ''
